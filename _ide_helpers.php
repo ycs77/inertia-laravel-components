c4f3bced7fc490b297ed3d6bc41d03cad0f21968
Lucas Yang <yangchenshin77@gmail.com>
2023-03-07 18:28:50 +0800

    Revert IDE helper file

---
+++ _ide_helpers.php
@@ -0,0 +1,27 @@
+<?php
+
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpFullyQualifiedNameUsageInspection */
+/** @noinspection PhpUnusedAliasInspection */
+
+namespace Illuminate\Support\Facades {
+
+    /**
+     * @method static self inertia(string $uri, string $component, array $props = [])
+     */
+    class Route
+    {
+        //
+    }
+}
+
+namespace Illuminate\Routing {
+
+    /**
+     * @method self inertia(string $uri, string $component, array $props = [])
+     */
+    class Router
+    {
+        //
+    }
+}
